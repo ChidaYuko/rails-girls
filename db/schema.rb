ad0325569f02aafe299a1f27585efaecf10ed2b1
ChidaYuko <chida.yuko.vareal@gmail.com>
2019-05-27 12:45:54 +0900

    evaluate and animation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_24_021610) do
+ActiveRecord::Schema.define(version: 2019_05_27_010615) do
 
   create_table "comments", force: :cascade do |t|
     t.string "user_name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2019_05_24_021610) do
     t.string "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "evaluation"
   end
 
   create_table "users", force: :cascade do |t|
